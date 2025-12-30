c0cfb66c9b5fad6a661f743a7c242bc1d7ae20af
Ben Andrews <bandrews-github@sendxml.com>
2025-12-29 22:45:26 -0800

    Fix ctrl+click to alt+click

--- basicpart/vite.config.js
+++ basicpart/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { copyFileSync } from 'fs';
+
+// Plugin to copy index.html to 404.html for SPA routing on static hosts
+const spa404Plugin = () => ({
+	name: 'spa-404',
+	closeBundle() {
+		copyFileSync('dist/index.html', 'dist/404.html');
+		console.log('Created 404.html for SPA routing');
+	}
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact(), spa404Plugin()],
 });
